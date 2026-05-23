8cb9160dc6ef2f5815a63d3a8ad034e7e42a3147
Mamaksimaaa <hajinertym@gmail.com>
2026-05-23 23:23:57 +0300

    Add comment for key retrieval in AIMBOT script
    Added a comment regarding key retrieval link.

--- UNIVERSAL/AIMBOT&ESP keyless.lua
+++ UNIVERSAL/AIMBOT&ESP keyless.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Mamaksimaaa/Roblox-script/refs/heads/main/UNIVERSAL/AIMBOT%26ESP%20keyless/AIMBOT%26ESP%20keyless.lua"))()
+--get key in https://link-hub.net/5957429/0owtgcnMnMTU
