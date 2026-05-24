f80a3f1131c0c25ce312ba44410a79a64b49d832
Mamaksimaaa <hajinertym@gmail.com>
2026-05-25 00:11:37 +0300

    Load external script in SCRIPT.lua
    Load an external script from a URL and add a comment for key retrieval.

---
+++ EVADE/SCRIPT.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mamaksimaaa/Roblox-script/refs/heads/main/EVADE/SRC/obfuscated_script-1779656933489.lua.txt"))()
+-- get key in https://link-hub.net/5957429/0owtgcnMnMTU
