bf21e1e2fcff7da61cc8bd0938901d150b0bc704
Mamaksimaaa <hajinertym@gmail.com>
2026-08-15 04:21:34 +0300

    Delete UNIVERSAL directory

--- UNIVERSAL/AIMBOT&ESP keyless.lua
+++
@@ -1,2 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mamaksimaaa/Roblox-script/refs/heads/main/UNIVERSAL/AIMBOT%26ESP%20keyless/AIMBOT%26ESP%20keyless.lua"))()
--- get key in https://link-hub.net/5957429/0owtgcnMnMTU
